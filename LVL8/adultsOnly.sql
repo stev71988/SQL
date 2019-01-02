bd896a8d37f2994fcdf5f561073da6c3bd76541c
stev71988 <stephensim@Stephens-MBP.fios-router.home>
2019-01-02 15:04:36 -0500

    first submissions

---
+++ LVL8/adultsOnly.sql
@@ -0,0 +1,12 @@
+-- CODE 
+SELECT * FROM users
+WHERE age >= 18;
+
+-- DESCRIPTION
+/* In your application, there is a section for adults only. You need to get a list of names and ages of users from the users table, who are 18 years old or older.
+
+users table schema
+
+name
+age
+*/
