a5276d0c262231f6a83e579d0470e71f16346029
stev71988 <stephensim@Stephens-MBP.fios-router.home>
2019-01-02 15:14:20 -0500

    first submission using INTERT INTO & VALUES

---
+++ LVL8/registerParty.sql
@@ -0,0 +1,14 @@
+-- CODE
+INSERT INTO participants
+VALUES ('Stephen', 30, true);
+
+
+-- DESCRIPTION
+/* You received an invitation to an amazing party. Now you need to write an insert statement to add yourself to the table of participants.
+
+participants table schema
+
+name (string)
+age (integer)
+attending (boolean)
+*/
