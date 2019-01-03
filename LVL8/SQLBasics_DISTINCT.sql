93e8aed3239007bbe36a1399ebabd9995db52cc4
stev71988 <stephensim@Stephens-MBP.fios-router.home>
2019-01-03 14:29:47 -0500

    first submission

---
+++ LVL8/SQLBasics_DISTINCT.sql
@@ -0,0 +1,16 @@
+-- CODE
+SELECT DISTINCT age FROM people
+
+
+-- DESCRIPTION
+/*
+For this challenge you need to create a simple DISTINCT statement, you want to find all the unique ages.
+
+people table schema
+id
+name
+age
+select table schema
+age (distinct)
+NOTE: Your solution should use pure SQL. Ruby is used within the test cases to do the actual testing.
+*/
